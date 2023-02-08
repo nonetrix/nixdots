7ad9ede7b400a55fa740392b8e82ce01bbdea170
Nonetrix <nonetrix@protonmail.com>
2023-02-07 23:43:50 -0600

    Blocked more garbage in hosts file nothing of value of lost

--- misc/hosts/hosts.nix
+++ misc/hosts/hosts.nix
@@ -2,8 +2,24 @@
 {
   # Host file configuration
   networking.extraHosts = let
-    # Download AdBlocker hosts file at https://github.com/StevenBlack/hosts with adware + malware + gambling
-    hostsPath = https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts;
-    hostsFile = builtins.fetchurl hostsPath;
-  in builtins.readFile "${hostsFile}";
+    # List of hosts file URLs
+    hostsList = [
+      "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts"
+      "https://www.github.developerdan.com/hosts/lists/dating-services-extended.txt
+      "https://www.github.developerdan.com/hosts/lists/amp-hosts-extended.txt""
+      "https://blocklistproject.github.io/Lists/facebook.txt"
+      "https://blocklistproject.github.io/Lists/tiktok.txt"
+      "https://blocklistproject.github.io/Lists/abuse.txt"
+      "https://blocklistproject.github.io/Lists/fraud.txt"
+      "https://blocklistproject.github.io/Lists/phishing.txt"
+      "https://blocklistproject.github.io/Lists/redirect.txt"
+      "https://blocklistproject.github.io/Lists/scam.txt"
+      "https://blocklistproject.github.io/Lists/ransomware.txt"
+    ];
+    # Iterate through the list of URLs
+    hosts = builtins.map (url: let
+      # Download the hosts file from each URL
+      hostsFile = builtins.fetchurl url;
+    in builtins.readFile "${hostsFile}") hostsList;
+  in builtins.concatStringsSep "\n" hosts;
 }
