c43a66d9cc834ecb14e0429b3937a9a423df8dcc
Nonetrix <nonetrix@protonmail.com>
2023-02-09 02:38:38 -0600

    Reduced audio latency

--- services/pipewire/pipewire.nix
+++ services/pipewire/pipewire.nix
@@ -15,9 +15,9 @@
         "link.max-buffers" = 16;
         "log.level" = 2;
         "default.clock.rate" = 48000;
-        "default.clock.quantum" = 32;
-        "default.clock.min-quantum" = 32;
-        "default.clock.max-quantum" = 32;
+        "default.clock.quantum" = 256;
+        "default.clock.min-quantum" = 256;
+        "default.clock.max-quantum" = 256;
         "core.daemon" = true;
         "core.name" = "pipewire-0";
       };
@@ -51,6 +51,43 @@
         { name = "libpipewire-module-link-factory"; }
         { name = "libpipewire-module-session-manager"; }
       ];
+    };
+
+      config.pipewire-pulse = {
+      "context.properties" = {
+        "log.level" = 2;
+      };
+      "context.modules" = [
+        {
+          name = "libpipewire-module-rtkit";
+          args = {
+            "nice.level" = -15;
+            "rt.prio" = 88;
+            "rt.time.soft" = 200000;
+            "rt.time.hard" = 200000;
+          };
+          flags = [ "ifexists" "nofail" ];
+        }
+        { name = "libpipewire-module-protocol-native"; }
+        { name = "libpipewire-module-client-node"; }
+        { name = "libpipewire-module-adapter"; }
+        { name = "libpipewire-module-metadata"; }
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            "pulse.min.req" = "256/48000";
+            "pulse.default.req" = "256/48000";
+            "pulse.max.req" = "256/48000";
+            "pulse.min.quantum" = "256/48000";
+            "pulse.max.quantum" = "256/48000";
+            "server.address" = [ "unix:native" ];
+          };
+        }
+      ];
+      "stream.properties" = {
+        "node.latency" = "256/48000";
+        "resample.quality" = 1;
+      };
     };
   };
 }
