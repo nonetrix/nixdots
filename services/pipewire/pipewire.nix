c4e06ae0ee946d88f76897b8dd80220f9af0e862
Nonetrix <nonetrix@protonmail.com>
2023-02-09 02:14:46 -0600

    Reduced audio latency

--- services/pipewire/pipewire.nix
+++ services/pipewire/pipewire.nix
@@ -1,14 +1,56 @@
 { config, pkgs, ... }:
 {
-    services = {
-      # Pipewire configuration
-      pipewire = {
-        enable = true;
-        # Enable Pipewire audio support
-        alsa.enable = true;
-        # Enable 32Bit Alsa libs, this is needed for many Steam games
-        alsa.support32Bit = true;
-        pulse.enable = true;
+  services.pipewire = {
+
+
+    enable = true;
+    # Enable Pipewire audio support
+    alsa.enable = true;
+    # Enable 32Bit Alsa libs, this is needed for many Steam games
+    alsa.support32Bit = true;
+    pulse.enable = true;
+
+    config.pipewire = {
+      "context.properties" = {
+        "link.max-buffers" = 16;
+        "log.level" = 2;
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 32;
+        "default.clock.min-quantum" = 32;
+        "default.clock.max-quantum" = 32;
+        "core.daemon" = true;
+        "core.name" = "pipewire-0";
       };
+      "context.modules" = [
+        {
+          name = "libpipewire-module-rtkit";
+          args = {
+            "nice.level" = -15;
+            "rt.prio" = 88;
+            "rt.time.soft" = 200000;
+            "rt.time.hard" = 200000;
+          };
+          flags = [ "ifexists" "nofail" ];
+        }
+        { name = "libpipewire-module-protocol-native"; }
+        { name = "libpipewire-module-profiler"; }
+        { name = "libpipewire-module-metadata"; }
+        { name = "libpipewire-module-spa-device-factory"; }
+        { name = "libpipewire-module-spa-node-factory"; }
+        { name = "libpipewire-module-client-node"; }
+        { name = "libpipewire-module-client-device"; }
+        {
+          name = "libpipewire-module-portal";
+          flags = [ "ifexists" "nofail" ];
+        }
+        {
+          name = "libpipewire-module-access";
+          args = {};
+        }
+        { name = "libpipewire-module-adapter"; }
+        { name = "libpipewire-module-link-factory"; }
+        { name = "libpipewire-module-session-manager"; }
+      ];
     };
+  };
 }
