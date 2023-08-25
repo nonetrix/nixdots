2645180e9a2aa8c8ed0d306c6837b83d73ccfa5d
fuggy <45698918+nonetrix@users.noreply.github.com>
2023-08-25 03:20:01 -0500

    Update hardware-configuration.nix
    Oopsie

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -6,7 +6,6 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
