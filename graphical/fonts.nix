410a310f548ca1c57a34958fe2d2c652c89a93ca
nonetrix <nonetrix@protonmail.com>
2024-03-08 16:48:54 -0600

    Clean fonts.nix

--- graphical/fonts.nix
+++ graphical/fonts.nix
@@ -3,14 +3,16 @@
   pkgs,
   ...
 }: {
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-extra
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    (nerdfonts.override {fonts = ["JetBrainsMono"];})
-    noto-fonts-emoji
-  ];
+  fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-extra
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      (nerdfonts.override {fonts = ["JetBrainsMono"];})
+      noto-fonts-emoji
+    ];
 
-  fonts.fontDir.enable = true;
+    fontDir.enable = true;
+  };
 }
