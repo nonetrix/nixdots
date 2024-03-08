00d4b72edfa06bacd7e0a6b61eca0fcd76527fc7
nonetrix <nonetrix@protonmail.com>
2024-03-08 17:17:37 -0600

    Clean zsh.nix

--- terminal/zsh.nix
+++ terminal/zsh.nix
@@ -7,60 +7,64 @@
 in {
   programs.zsh.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    thefuck
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      thefuck
+    ];
 
-  environment.variables = {
-    MOZ_ENABLE_WAYLAND = "1";
-    EDITOR = "nvim";
-    NIXOS_OZONE_WL = "1";
+    variables = {
+      MOZ_ENABLE_WAYLAND = "1";
+      EDITOR = "nvim";
+      NIXOS_OZONE_WL = "1";
+    };
   };
 
   home-manager.users.${global.username} = {pkgs, ...}: {
-    programs.starship = {
-      enable = true;
-    };
-
-    programs.zoxide = {
-      enable = true;
-    };
-
-    programs.zsh = {
-      enable = true;
-
-      history.ignoreDups = true;
-
-      # Aliases
-      shellAliases = {
-        "ls" = "lsd ";
-        "cat" = "bat -pp ";
-        "grep" = "rg ";
-        ".." = "cd ..";
-        "tp" = "trash put";
-        "radar" = "curl https://sirocco.accuweather.com/nx_mosaic_640x480_public/sir/inmasiral_.gif | imv -";
-        "forecast" = "curl wttr.in";
-        "umatrix" = "unimatrix";
-        "gsrun" = "gamescope -W 1920 -H 1080 -r 165";
+    programs = {
+      starship = {
+        enable = true;
       };
 
-      initExtraFirst = ''
-        fastfetch
-        export PATH="$HOME/.local/bin:$PATH"
-        eval $(thefuck --alias)
-
-      '';
-
-      syntaxHighlighting.enable = true;
-      enableAutosuggestions = true;
-      zplug = {
+      zoxide = {
         enable = true;
-        plugins = [
-          {name = "catppuccin/zsh-syntax-highlighting";}
-          {name = "jeffreytse/zsh-vi-mode";}
-          {name = "chisui/zsh-nix-shell";}
-          {name = "nix-community/nix-zsh-completions";}
-        ];
+      };
+
+      zsh = {
+        enable = true;
+
+        history.ignoreDups = true;
+
+        # Aliases
+        shellAliases = {
+          "ls" = "lsd ";
+          "cat" = "bat -pp ";
+          "grep" = "rg ";
+          ".." = "cd ..";
+          "tp" = "trash put";
+          "radar" = "curl https://sirocco.accuweather.com/nx_mosaic_640x480_public/sir/inmasiral_.gif | imv -";
+          "forecast" = "curl wttr.in";
+          "umatrix" = "unimatrix";
+          "gsrun" = "gamescope -W 1920 -H 1080 -r 165";
+        };
+
+        initExtraFirst = ''
+          fastfetch
+          export PATH="$HOME/.local/bin:$PATH"
+          eval $(thefuck --alias)
+
+        '';
+
+        syntaxHighlighting.enable = true;
+        enableAutosuggestions = true;
+        zplug = {
+          enable = true;
+          plugins = [
+            {name = "catppuccin/zsh-syntax-highlighting";}
+            {name = "jeffreytse/zsh-vi-mode";}
+            {name = "chisui/zsh-nix-shell";}
+            {name = "nix-community/nix-zsh-completions";}
+          ];
+        };
       };
     };
   };
