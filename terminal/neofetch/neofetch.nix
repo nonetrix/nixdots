064bb22aac17da972e4d01e2ccf75df4fda9bc72
nonetrix <nonetrix@protonmail.com>
2024-03-13 23:35:45 -0500

    Improve neofetch configs

--- terminal/neofetch/neofetch.nix
+++ terminal/neofetch/neofetch.nix
@@ -6,31 +6,31 @@ in {
 
     home.file.".config/neofetch/config.conf".text = ''
       print_info () {
-      prin "┌──────────────────────────────────────────────────────┐"
+      prin "🌟 ♥𖹭♥♥❤❤♥❤𖹭❤𖹭❤𖹭❤❤❤𖹭𖹭❤♥❤𖹭𖹭♥❤♥♥♥♥❤𖹭❤❤𖹭𖹭♥𖹭♥❤♥❤♥❤𖹭𖹭♥❤♥❤❤❤❤𖹭❤❤𖹭 ✨"
       info title
 
-      info "󰍹" model
-      info "󱄅" distro
-      info "" kernel
-      info "󰄉" uptime
-      info "" packages
-      info "" shell
-      info "󰨇" resolution
-      info "󰧨" de
-      info "󰧨" wm
-      info "󰃣" theme
-      info "󱗿" icons
-      info "" term
-      info "" term_font
-      info "" cpu
-      info "󰾲" gpu
-      info "󰍛" memory
-      prin "└──────────────────────────────────────────────────────┘"
+      info "󰍹 " model
+      info "󱄅 " distro
+      info " " kernel
+      info "󰄉 " uptime
+      info " " packages
+      info " " shell
+      info "󰨇 " resolution
+      info "󰧨 " de
+      info "󰧨 " wm
+      info "󰃣 " theme
+      info "󱗿 " icons
+      info " " term
+      info " " term_font
+      info " " cpu
+      info "󰾲 " gpu
+      info "󰍛 " memory
+      prin "✨ 𖹭♥𖹭♥❤❤♥❤𖹭𖹭❤♥♥❤❤❤𖹭❤𖹭𖹭❤❤❤❤❤𖹭♥𖹭❤𖹭𖹭❤♥❤❤♥𖹭❤𖹭♥♥❤♥♥❤♥𖹭♥𖹭❤❤♥♥❤𖹭♥ 🌟"
   }
 
   image_backend="iterm2"
   image_source="${./fetch.png}"
-  image_size="450"
+  image_size="370"
   '';
   };
 }
